0896990a49725f918fc8599b93c6392cd189f7ff
JonathanDP02 <jonatanpangestu23@gmail.com>
2024-10-02 22:14:46 +0700

    pertanyaan1

--- PemilihanBilangan07.java
+++ PemilihanBilangan07.java
@@ -8,13 +8,16 @@ public class PemilihanBilangan07 {
         System.out.print("masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0) 
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");           
-        }
-        else
-        {
-            System.out.println("Angka " + angka + "termasuk bilangan ganjil");
-        }
+       // if (angka % 2 == 0) 
+       // {
+       //     System.out.println("Angka " + angka + " termasuk bilangan genap");           
+       // }
+       // else
+       // {
+       //     System.out.println("Angka " + angka + "termasuk bilangan ganjil");
+       // }
+
+       String hasil = (angka % 2 == 0) ? angka + " adalah bilangan genap" : angka + " adalah bilangan ganjil";
+       System.out.println(hasil);
     }
 }
